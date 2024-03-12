f9c81e8522a9497c2e2244cce0451288811c63d2
TheNotEasy <sasha.antonov092@gmail.com>
2024-03-12 22:32:44 +0900

    Switch to HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './App.scss'
-import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
+import {HashRouter, Navigate, Route, Routes} from "react-router-dom";
 import {Auth} from "@/pages/Auth/Auth.tsx";
 import {SignUp} from "@/pages/SignUp/SignUp.tsx";
 import {Header} from "@/components/Header/Header.tsx";
@@ -8,7 +8,7 @@ import {LessonPicker} from "@/pages/LessonPicker/LessonPicker.tsx";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Header />
       <Routes>
         <Route path="/" element={<Navigate to={"/auth"} />} />
@@ -17,7 +17,7 @@ function App() {
         <Route path="/classroom" element={<Classroom />}></Route>
         <Route path="/lesson-picker" element={<LessonPicker />}></Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
